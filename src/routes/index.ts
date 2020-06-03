3400c2ad0bc5853db8cac79bd04a1249769a2817
Claudio Castro <claudioluiz.castro@gmail.com>
2020-06-03 00:21:33 -0300

    create items route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import Knex from 'knex';
 
 const routes = express.Router();
 
 routes.get('/', (request, response) => {
   response.json({ name: 'Cláudio' });
+});
+
+routes.get('/items', async (request, response) => {
+  const items = await Knex('items').select('*');
+
+  return response.json(items);
 });
 
 export default routes;
